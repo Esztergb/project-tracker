f78e1da80299093fcd8c47f4c1ddd193d960cf14
Esztergb <eszterbarbuscia@Eszters-MacBook-Air.local>
2023-05-04 17:58:31 -0700

    spacing

--- README.md
+++ README.md
@@ -21,9 +21,9 @@ THEN each timeblock is color coded to indicate whether it is in the past, presen
 WHEN I click into a timeblock
 THEN I can enter an event (DONE)
 WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
+THEN the text for that event is saved in local storage (DONE)
 WHEN I refresh the page
-THEN the saved events persist
+THEN the saved events persist (DONE)
 ```
 
 ## Description
@@ -35,17 +35,16 @@ See under User Story
 The link to deployed application is: https://esztergb.github.io/project-tracker/
 Github link: https://github.com/Esztergb/project-tracker.git
 
-![ScreenShot](assets/images/screenshotfull.png)
+![ScreenShot](assets/images/Screenshot.png)
 
 ## Usage
 
-
+N/A 
 
 ## Credits
 
 Thank you for tips and sugestions from Bootcamp instructors and classmates. 
 I have used https://www.w3schools.com/ and https://www.stackoverflow.com to research information.
-I have reviewed you tube videos from Ania Kubow to help me with this project. 
 
 ## License
 
--- assets/script.js
+++ assets/script.js
@@ -1,64 +1,57 @@
-//========================Date and time on header============================
-// //setting time with DayJS
-// $('#currentDay').text(dayjs());
-
-// currentDate = $("#currentDay")
-// //update display to the following format
-// function todaysDate (){
-//   currentDate.text(dayjs().format("[Today is] dddd, MMM DD, YYYY,HH:mm:ss"));
-// }
-// todaysDate();
-// //update time evey second
-// setInterval(todaysDate, 1000);
-
-
-function displayTime(){
-var today = dayjs();
-//Day of the week today 
-var dayWeek = today.format("[Today is] dddd, MMM DD, YYYY");
-$('#currentDay').text(dayWeek);
-//Current time
-var currentTime = today.format("HH:mm:ss")
-$('#time').text(currentTime);
-}
-displayTime()
+$(function () {
+  //========================Date and time on header============================
+  //setting time with DayJS
+  function displayTime() {
+    var today = dayjs();
+    //Day of the week today
+    var dayWeek = today.format("[Today is] dddd, MMM DD, YYYY");
+    $("#currentDay").text(dayWeek);
+    //Current time
+    var currentTime = today.format("HH:mm:ss");
+    $("#time").text(currentTime);
+  }
+  displayTime();
 
 setInterval(displayTime, 1000);
 
-//========================set local storage==================================
-
+  //========================set local storage==================================
 $(document).ready(function () {
-// listen for save button clicks
+    // listen for save button clicks
     $(".saveBtn").on("click", function () {
-// get nearby values
-      var value = $(this).siblings(".description").val();
-      var time = $(this).parent().attr("id");
-// save in localStorage
-      localStorage.setItem(time, value);
-});
-  
-//=============function to change background color based on time=============
-  
+    // get nearby values
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    // save in localStorage
+    localStorage.setItem(time, value);
+    });
+  });
+
+  //=============function to change background color based on time=============
 function changeBackground() {
   var currentHour = dayjs().format("H");
-//console.log(currentHour)
+  //console.log(currentHour)
 
- $("textarea.description").each(function (i) {  //The each() method specifies a function to run for each matched element
+  $("textarea.description").each(function (i) {
+  //The each() method specifies a function to run for each matched element
   var inputTime = parseInt($(this).attr("id")); //parseInt Converts a string to an integer
-//console.log(inputTime)
+  //console.log(inputTime)
 
-//if the time is < than currentHour add PAST class to time block class
-      if (inputTime < currentHour) {$(this).addClass("past");
-//else if time === currentHour add PRESENT class to time block
-      } else if (inputTime == currentHour) {$(this).addClass("present");
-//else add FUTURE class to time block
-      } else {$(this).addClass("future");
+  //if the time is < than currentHour add PAST class to time block class
+  if (inputTime < currentHour) {
+      $(this).addClass("past");
+      //else if time === currentHour add PRESENT class to time block
+      } else if (inputTime == currentHour) {
+        $(this).addClass("present");
+        //else add FUTURE class to time block
+      } else {
+        $(this).addClass("future");
       }
-    })
+  });
 }
-changeBackground()
+changeBackground();
 
-// // load any saved data from localStorage
+  // load any saved data from localStorage
+
 $("#hour-9 .description").val(localStorage.getItem("hour-9"));
 $("#hour-10 .description").val(localStorage.getItem("hour-10"));
 $("#hour-11 .description").val(localStorage.getItem("hour-11"));
@@ -69,7 +62,27 @@ $("#hour-15 .description").val(localStorage.getItem("hour-15"));
 $("#hour-16 .description").val(localStorage.getItem("hour-16"));
 $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-// set up interval to check if current time needs to be updated
-setInterval(changeBackground, 1000);
+//auto reload document every 5 minutes
+setInterval (function(){
+      window.location.reload();
+}, 300000);
+
+//how to clear the calendar at the end of the day - worked on this with Tutor
+// function checkIfNewDay(){
+//       var today = dayjs();
+//       var lastWorkingDay = localStorage.getItem("last-working-day")
+//       if (lastWorkingDay) {
+//             var endOfLastWorkingDay = dayjs(lastWorkingDay).endOf('day')
+            
+//             if (today.isAfter(endOfLastWorkingDay)) {
+//                   localStorage.clear()
+//                   localStorage.setItem("last-working-day", today.format());
+//             }
+//       } else {
+//             localStorage.setItem('last-working-day', today.format())
+//       }
+
+// }
+// checkIfNewDay()
 
 });
